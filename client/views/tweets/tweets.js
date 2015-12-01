749bd44de523d706073af53e68678bbd5218ad9c
illya nizyev <nzv@protonmail.ch>
2015-12-01 19:15:33 +0200

    twitter feed helpers

--- client/views/tweets/tweets.js
+++ client/views/tweets/tweets.js
@@ -1,18 +1,7 @@
-Template.tweets.rendered = function () {
-  window.twttr = (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
+Template.twitterfeed.helpers({
+  tweets: function(){
+    return Tweets.find({});
+  }
+});
 
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-
-    return t;
-  }(document, "script", "twitter-wjs"));
-}
+Meteor.subscribe('tweets');
